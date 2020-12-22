ae338672fb009f8bc50ac8fc853461bb7e71b069
msmk530 <msmk530@naver.com>
2020-12-22 17:09:06 +0900

    [feat] game판에 cell 배치

--- react-tetris/src/components/Cell.js
+++ react-tetris/src/components/Cell.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../tetrominos';
 const Cell = ({ type }) => (
-  <StyledCell type={type} color={TETROMINOS[type].color}>
-    cell
-  </StyledCell>
+  <StyledCell type={'L'} color={TETROMINOS['L'].color} />
 );
 
 export default Cell;
