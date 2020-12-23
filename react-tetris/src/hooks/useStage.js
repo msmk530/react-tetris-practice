b50659629eff5500391a13c1b0e1e08db684dff6
msmk530 <msmk530@naver.com>
2020-12-23 14:45:33 +0900

    [fix] reduce배열 return 하도록 수정 - 중괄호와 return을 함께 생략할 수 있지만 통일성을 위해 넣어줌

--- react-tetris/src/hooks/useStage.js
+++ react-tetris/src/hooks/useStage.js
@@ -10,7 +10,7 @@ export const useStage = (player, resetPlayer) => {
     setRowsCleared(0);
 
     const sweepRows = (newStage) => {
-      newStage.reduce((ack, row) => {
+      return newStage.reduce((ack, row) => {
         if (row.findIndex((cell) => cell[0] === 0) === -1) {
           setRowsCleared((perv) => perv + 1);
           ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
@@ -20,7 +20,6 @@ export const useStage = (player, resetPlayer) => {
         return ack;
       }, []);
     };
-
     const updateStage = (prevStage) => {
       const newStage = prevStage.map((row) =>
         row.map((cell) => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
