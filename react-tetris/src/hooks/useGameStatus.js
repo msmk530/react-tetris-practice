e7fec9cd9963a51851bcaecc5994c11af2c981b7
msmk530 <msmk530@naver.com>
2020-12-23 15:24:37 +0900

    [fix] hooks 이름 오타 수정

--- react-tetris/src/hooks/useGameStatus.js
+++ react-tetris/src/hooks/useGameStatus.js
@@ -1,9 +1,9 @@
-import { useStage, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 export const useGameStatus = (rowsCleared) => {
-  const [score, setScore] = useStage(0);
-  const [rows, setRows] = useStage(0);
-  const [level, setLevel] = useStage(0);
+  const [score, setScore] = useState(0);
+  const [rows, setRows] = useState(0);
+  const [level, setLevel] = useState(0);
 
   const linePoints = [40, 100, 300, 1200];
 
